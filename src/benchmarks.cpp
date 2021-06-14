5836832186924f48fff04c12d34033cf9ec19c87
Dominik Horn <dominik.horn@tum.de>
2021-06-14 12:05:33 +0200

    implement proper throughput benchmark
    * custom dataset
    * convenience wrapped for easier access

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -1,40 +1,57 @@
+#include <cstdint>
 #include <iostream>
+#include <random>
+#include <string>
 
 #include <hashing.hpp>
 #include <benchmark/benchmark.h>
 
-template<class Hashfn, class Reductionfn>
-void BM_THROUGHPUT(benchmark::State& state) {
-   Hashfn hashfn;
-   Reductionfn reductionfn(200000000);
-
-   // TODO: hash actual dataset
-   std::uint64_t key = 0;
+template<class Hashfn, class Reductionfn, class Data>
+auto __BM_throughput = [](benchmark::State& state, const std::vector<Data>* dataset) {
+   const Hashfn hashfn;
+   const Reductionfn reductionfn(dataset->size());
 
    for (auto _ : state) {
-      const auto hash = hashfn(key);
-      const auto index = reductionfn(hash);
-      benchmark::DoNotOptimize(index);
-      key++;
+      for (const auto& key : *dataset) {
+         const auto hash = hashfn(key);
+         const auto index = reductionfn(hash);
+         benchmark::DoNotOptimize(index);
+      }
    }
+
+   state.SetLabel(Hashfn::name() + ":" + Reductionfn::name());
+   state.SetItemsProcessed(dataset->size());
+   state.SetBytesProcessed(dataset->size() * sizeof(Data));
+};
+
+#define BENCHMARK_THROUGHPUT(Hashfn, dataset)                                                                        \
+   benchmark::RegisterBenchmark(                                                                                     \
+      "throughput", __BM_throughput<Hashfn, hashing::reduction::Fastrange<HASH_32>, decltype(dataset)::value_type>,  \
+      &dataset);                                                                                                     \
+   benchmark::RegisterBenchmark(                                                                                     \
+      "throughput", __BM_throughput<Hashfn, hashing::reduction::Fastrange<HASH_64>, decltype(dataset)::value_type>,  \
+      &dataset);                                                                                                     \
+   benchmark::RegisterBenchmark(                                                                                     \
+      "throughput", __BM_throughput<Hashfn, hashing::reduction::FastModulo<HASH_32>, decltype(dataset)::value_type>, \
+      &dataset);                                                                                                     \
+   benchmark::RegisterBenchmark(                                                                                     \
+      "throughput", __BM_throughput<Hashfn, hashing::reduction::FastModulo<HASH_64>, decltype(dataset)::value_type>, \
+      &dataset);
+
+int main(int argc, char** argv) {
+   const size_t dataset_size = 100000000;
+   std::vector<std::uint64_t> dataset;
+
+   // generate uniform random numbers dataset
+   std::default_random_engine rng_gen;
+   std::uniform_int_distribution<decltype(dataset)::value_type> dist(0, static_cast<size_t>(0x1) << 50);
+   dataset.resize(dataset_size);
+   for (size_t i = 0; i < dataset_size; i++)
+      dataset[i] = dist(rng_gen);
+
+   BENCHMARK_THROUGHPUT(hashing::MultPrime32, dataset);
+
+   benchmark::Initialize(&argc, argv);
+   benchmark::RunSpecifiedBenchmarks();
+   benchmark::Shutdown();
 }
-
-using namespace hashing;
-using namespace reduction;
-
-#define BENCHMARK_THROUGHPUT(Hashfn)                               \
-   BENCHMARK_TEMPLATE(BM_THROUGHPUT, Hashfn, Fastrange<HASH_32>);  \
-   BENCHMARK_TEMPLATE(BM_THROUGHPUT, Hashfn, Fastrange<HASH_64>);  \
-   BENCHMARK_TEMPLATE(BM_THROUGHPUT, Hashfn, FastModulo<HASH_32>); \
-   BENCHMARK_TEMPLATE(BM_THROUGHPUT, Hashfn, FastModulo<HASH_64>);
-
-// Register benchmarks
-BENCHMARK_THROUGHPUT(MultPrime32)
-BENCHMARK_THROUGHPUT(MultPrime64)
-BENCHMARK_THROUGHPUT(MurmurFinalizer<HASH_32>)
-BENCHMARK_THROUGHPUT(MurmurFinalizer<HASH_64>)
-BENCHMARK_THROUGHPUT(AquaHash<HASH_32>)
-BENCHMARK_THROUGHPUT(AquaHash<HASH_64>)
-
-// Run the benchmarks
-BENCHMARK_MAIN();
