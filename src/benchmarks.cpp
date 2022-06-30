d3064fcf762cbf60f32163d3cd95d915b388fe72
Dominik Horn <dominik.horn@tum.de>
2022-06-30 16:27:50 +0200

    implement collision benchmarks

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -24,6 +24,31 @@ auto __BM_throughput = [](benchmark::State& state, const std::vector<Data>& data
    state.SetBytesProcessed(dataset.size() * static_cast<size_t>(state.iterations()) * sizeof(Data));
 };
 
+template<class Hashfn, class Reductionfn, class Data>
+auto __BM_collisions = [](benchmark::State& state, const std::vector<Data>& dataset) {
+   const auto N = 1000;
+   std::array<size_t, N> buckets;
+   std::fill(buckets.begin(), buckets.end(), 0);
+
+   const Hashfn hashfn;
+   const Reductionfn reductionfn(N);
+
+   for (auto _ : state) {
+      for (const auto& key : dataset) {
+         const auto hash = hashfn(key);
+         const auto index = reductionfn(hash);
+         buckets[index]++;
+      }
+   }
+
+   for (size_t i = 0; i < N; i++)
+      state.counters["bucket_" + std::to_string(i)] = buckets[i];
+
+   state.SetLabel(Hashfn::name() + ":" + Reductionfn::name() + ":" + std::to_string(N));
+   state.SetItemsProcessed(dataset.size() * static_cast<size_t>(state.iterations()));
+   state.SetBytesProcessed(dataset.size() * static_cast<size_t>(state.iterations()) * sizeof(Data));
+};
+
 template<class Hashfn, class Data>
 auto __BM_biased_throughput = [](benchmark::State& state, const std::vector<Data>& dataset) {
    const Hashfn hashfn(dataset.size());
@@ -40,16 +65,43 @@ auto __BM_biased_throughput = [](benchmark::State& state, const std::vector<Data
    state.SetBytesProcessed(dataset.size() * static_cast<size_t>(state.iterations()) * sizeof(Data));
 };
 
-#define BENCHMARK_THROUGHPUT(Hashfn, dataset)                                                                          \
+template<class Hashfn, class Data>
+auto __BM_biased_collisions = [](benchmark::State& state, const std::vector<Data>& dataset) {
+   const auto N = 1000;
+   std::array<size_t, N> buckets;
+   std::fill(buckets.begin(), buckets.end(), 0);
+
+   const Hashfn hashfn(N);
+
+   for (auto _ : state) {
+      for (const auto& key : dataset) {
+         const auto index = hashfn(key);
+         buckets[index]++;
+      }
+   }
+
+   for (size_t i = 0; i < N; i++)
+      state.counters["bucket_" + std::to_string(i)] = buckets[i];
+
+   state.SetLabel(Hashfn::name() + ":" + std::to_string(N));
+   state.SetItemsProcessed(dataset.size() * static_cast<size_t>(state.iterations()));
+   state.SetBytesProcessed(dataset.size() * static_cast<size_t>(state.iterations()) * sizeof(Data));
+};
+
+#define BENCHMARK_UNIFORM(Hashfn, dataset)                                                                             \
    benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::DoNothing<T>, T>, dataset);  \
    benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::Fastrange<T>, T>, dataset);  \
    benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::Modulo<T>, T>, dataset);     \
    benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::FastModulo<T>, T>, dataset); \
    benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::BranchlessFastModulo<T>, T>, \
-                                dataset);
+                                dataset);                                                                              \
+   benchmark::RegisterBenchmark("collisions", __BM_collisions<Hashfn, hashing::reduction::Fastrange<T>, T>, dataset);  \
+   benchmark::RegisterBenchmark("collisions", __BM_collisions<Hashfn, hashing::reduction::Modulo<T>, T>, dataset);     \
+   benchmark::RegisterBenchmark("collisions", __BM_collisions<Hashfn, hashing::reduction::FastModulo<T>, T>, dataset);
 
-#define BENCHMARK_BIASED_THROUGHPUT(Hashfn, dataset) \
-   benchmark::RegisterBenchmark("throughput", __BM_biased_throughput<Hashfn, T>, dataset);
+#define BENCHMARK_BIASED(Hashfn, dataset)                                                  \
+   benchmark::RegisterBenchmark("throughput", __BM_biased_collisions<Hashfn, T>, dataset); \
+   benchmark::RegisterBenchmark("collisions", __BM_biased_collisions<Hashfn, T>, dataset);
 
 template<class T>
 void BenchmarkThroughput(const size_t& dataset_size) {
@@ -66,23 +118,23 @@ void BenchmarkThroughput(const size_t& dataset_size) {
 
    static_assert(sizeof(T) == sizeof(uint64_t) || sizeof(T) == sizeof(uint32_t), "unimplemented benchmark");
    if constexpr (sizeof(T) == sizeof(uint64_t)) {
-      BENCHMARK_BIASED_THROUGHPUT(hashing::MultPrime64, dataset);
-      BENCHMARK_BIASED_THROUGHPUT(hashing::Fibonacci64, dataset);
-      BENCHMARK_BIASED_THROUGHPUT(hashing::FibonacciPrime64, dataset);
-      BENCHMARK_THROUGHPUT(hashing::AquaHash<HASH_64>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::XXHash3<HASH_64>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::MurmurFinalizer<HASH_64>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::CityHash64<HASH_64>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::reduction::Lower<hashing::CityHash128<HASH_64>>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::reduction::Higher<hashing::CityHash128<HASH_64>>, dataset);
+      BENCHMARK_BIASED(hashing::MultPrime64, dataset);
+      BENCHMARK_BIASED(hashing::Fibonacci64, dataset);
+      BENCHMARK_BIASED(hashing::FibonacciPrime64, dataset);
+      BENCHMARK_UNIFORM(hashing::AquaHash<HASH_64>, dataset);
+      BENCHMARK_UNIFORM(hashing::XXHash3<HASH_64>, dataset);
+      BENCHMARK_UNIFORM(hashing::MurmurFinalizer<HASH_64>, dataset);
+      BENCHMARK_UNIFORM(hashing::CityHash64<HASH_64>, dataset);
+      BENCHMARK_UNIFORM(hashing::reduction::Lower<hashing::CityHash128<HASH_64>>, dataset);
+      BENCHMARK_UNIFORM(hashing::reduction::Higher<hashing::CityHash128<HASH_64>>, dataset);
    } else if constexpr (sizeof(T) == sizeof(uint32_t)) {
-      BENCHMARK_BIASED_THROUGHPUT(hashing::MultPrime32, dataset);
-      BENCHMARK_BIASED_THROUGHPUT(hashing::Fibonacci32, dataset);
-      BENCHMARK_BIASED_THROUGHPUT(hashing::FibonacciPrime32, dataset);
-      BENCHMARK_THROUGHPUT(hashing::AquaHash<HASH_32>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::XXHash3<HASH_32>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::MurmurFinalizer<HASH_32>, dataset);
-      BENCHMARK_THROUGHPUT(hashing::CityHash32<HASH_32>, dataset);
+      BENCHMARK_BIASED(hashing::MultPrime32, dataset);
+      BENCHMARK_BIASED(hashing::Fibonacci32, dataset);
+      BENCHMARK_BIASED(hashing::FibonacciPrime32, dataset);
+      BENCHMARK_UNIFORM(hashing::AquaHash<HASH_32>, dataset);
+      BENCHMARK_UNIFORM(hashing::XXHash3<HASH_32>, dataset);
+      BENCHMARK_UNIFORM(hashing::MurmurFinalizer<HASH_32>, dataset);
+      BENCHMARK_UNIFORM(hashing::CityHash32<HASH_32>, dataset);
    }
 }
 
