c29600fa5dbda84c8424d94af4ed3a560655866b
Dominik Horn <dominik.horn@tum.de>
2022-06-30 15:15:15 +0200

    run 32 and 64 bit benchmarks

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -7,12 +7,12 @@
 #include <benchmark/benchmark.h>
 
 template<class Hashfn, class Reductionfn, class Data>
-auto __BM_throughput = [](benchmark::State& state, const std::vector<Data>* dataset) {
+auto __BM_throughput = [](benchmark::State& state, const std::vector<Data>& dataset) {
    const Hashfn hashfn;
-   const Reductionfn reductionfn(dataset->size());
+   const Reductionfn reductionfn(dataset.size());
 
    for (auto _ : state) {
-      for (const auto& key : *dataset) {
+      for (const auto& key : dataset) {
          const auto hash = hashfn(key);
          const auto index = reductionfn(hash);
          benchmark::DoNotOptimize(index);
@@ -20,50 +20,73 @@ auto __BM_throughput = [](benchmark::State& state, const std::vector<Data>* data
    }
 
    state.SetLabel(Hashfn::name() + ":" + Reductionfn::name());
-   state.SetItemsProcessed(dataset->size());
-   state.SetBytesProcessed(dataset->size() * sizeof(Data));
+   state.SetItemsProcessed(dataset.size());
+   state.SetBytesProcessed(dataset.size() * sizeof(Data));
+};
+
+template<class Hashfn, class Data>
+auto __BM_biased_throughput = [](benchmark::State& state, const std::vector<Data>& dataset) {
+   const Hashfn hashfn(dataset.size());
+
+   for (auto _ : state) {
+      for (const auto& key : dataset) {
+         const auto index = hashfn(key);
+         benchmark::DoNotOptimize(index);
+      }
+   }
+
+   state.SetLabel(Hashfn::name());
+   state.SetItemsProcessed(dataset.size());
+   state.SetBytesProcessed(dataset.size() * sizeof(Data));
 };
 
 #define BENCHMARK_THROUGHPUT(Hashfn, dataset)                                                                          \
-   benchmark::RegisterBenchmark("throughput",                                                                          \
-                                __BM_throughput<Hashfn, hashing::reduction::DoNothing<decltype(dataset)::value_type>,  \
-                                                decltype(dataset)::value_type>,                                        \
-                                &dataset);                                                                             \
-   benchmark::RegisterBenchmark("throughput",                                                                          \
-                                __BM_throughput<Hashfn, hashing::reduction::Fastrange<decltype(dataset)::value_type>,  \
-                                                decltype(dataset)::value_type>,                                        \
-                                &dataset);                                                                             \
-   benchmark::RegisterBenchmark("throughput",                                                                          \
-                                __BM_throughput<Hashfn, hashing::reduction::Modulo<decltype(dataset)::value_type>,     \
-                                                decltype(dataset)::value_type>,                                        \
-                                &dataset);                                                                             \
-   benchmark::RegisterBenchmark("throughput",                                                                          \
-                                __BM_throughput<Hashfn, hashing::reduction::FastModulo<decltype(dataset)::value_type>, \
-                                                decltype(dataset)::value_type>,                                        \
-                                &dataset);                                                                             \
-   benchmark::RegisterBenchmark(                                                                                       \
-      "throughput",                                                                                                    \
-      __BM_throughput<Hashfn, hashing::reduction::BranchlessFastModulo<decltype(dataset)::value_type>,                 \
-                      decltype(dataset)::value_type>,                                                                  \
-      &dataset);
+   benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::DoNothing<T>, T>, dataset);  \
+   benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::Fastrange<T>, T>, dataset);  \
+   benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::Modulo<T>, T>, dataset);     \
+   benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::FastModulo<T>, T>, dataset); \
+   benchmark::RegisterBenchmark("throughput", __BM_throughput<Hashfn, hashing::reduction::BranchlessFastModulo<T>, T>, \
+                                dataset);
+
+#define BENCHMARK_BIASED_THROUGHPUT(Hashfn, dataset) \
+   benchmark::RegisterBenchmark("throughput", __BM_biased_throughput<Hashfn, T>, dataset);
+
+template<class T>
+void BenchmarkThroughput(const size_t& dataset_size) {
+   // generate uniform random numbers dataset
+   std::default_random_engine rng_gen(42);
+   std::uniform_int_distribution<T> dist(0, 1ULL << (sizeof(T) * 8 - 1));
+
+   std::vector<T> dataset;
+   dataset.reserve(dataset_size);
+   for (size_t i = 0; i < dataset_size; i++)
+      dataset.push_back(dist(rng_gen));
+
+   assert(dataset.size() == dataset_size);
+
+   static_assert(sizeof(T) == sizeof(uint64_t) || sizeof(T) == sizeof(uint32_t), "unimplemented benchmark");
+   if constexpr (sizeof(T) == sizeof(uint64_t)) {
+      BENCHMARK_BIASED_THROUGHPUT(hashing::MultPrime64, dataset);
+      BENCHMARK_BIASED_THROUGHPUT(hashing::Fibonacci64, dataset);
+      BENCHMARK_BIASED_THROUGHPUT(hashing::FibonacciPrime64, dataset);
+      BENCHMARK_THROUGHPUT(hashing::AquaHash<HASH_64>, dataset);
+      BENCHMARK_THROUGHPUT(hashing::XXHash3<HASH_64>, dataset);
+      BENCHMARK_THROUGHPUT(hashing::MurmurFinalizer<HASH_64>, dataset);
+   } else if constexpr (sizeof(T) == sizeof(uint32_t)) {
+      BENCHMARK_BIASED_THROUGHPUT(hashing::MultPrime32, dataset);
+      BENCHMARK_BIASED_THROUGHPUT(hashing::Fibonacci32, dataset);
+      BENCHMARK_BIASED_THROUGHPUT(hashing::FibonacciPrime32, dataset);
+      BENCHMARK_THROUGHPUT(hashing::AquaHash<HASH_32>, dataset);
+      BENCHMARK_THROUGHPUT(hashing::XXHash3<HASH_32>, dataset);
+      BENCHMARK_THROUGHPUT(hashing::MurmurFinalizer<HASH_32>, dataset);
+   }
+}
 
 int main(int argc, char** argv) {
-   const size_t dataset_size = 200000000;
-   std::vector<std::uint64_t> dataset;
+   const size_t dataset_size = 500'000'000ULL;
 
-   // generate uniform random numbers dataset
-   std::default_random_engine rng_gen;
-   std::uniform_int_distribution<decltype(dataset)::value_type> dist(0, static_cast<size_t>(0x1) << 50);
-   dataset.resize(dataset_size);
-   for (size_t i = 0; i < dataset_size; i++)
-      dataset[i] = dist(rng_gen);
-
-   BENCHMARK_THROUGHPUT(hashing::MultPrime64, dataset);
-   BENCHMARK_THROUGHPUT(hashing::Fibonacci64, dataset);
-   BENCHMARK_THROUGHPUT(hashing::FibonacciPrime64, dataset);
-   BENCHMARK_THROUGHPUT(hashing::AquaHash<HASH_64>, dataset);
-   BENCHMARK_THROUGHPUT(hashing::XXHash3<HASH_64>, dataset);
-   BENCHMARK_THROUGHPUT(hashing::MurmurFinalizer<HASH_64>, dataset);
+   BenchmarkThroughput<uint32_t>(dataset_size);
+   BenchmarkThroughput<uint64_t>(dataset_size);
 
    benchmark::Initialize(&argc, argv);
    benchmark::RunSpecifiedBenchmarks();
